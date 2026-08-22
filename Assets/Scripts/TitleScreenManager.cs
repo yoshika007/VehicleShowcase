bfa2b47d95a5b9c3081c3bd9b9323d0e5ffed3d1
Yoshikazu Tanami <peanuts.yoshika.0707@gmail.com>
2026-08-22 16:33:53 +0900

    Create title screen and scene navigation

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void StartShowcase()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
